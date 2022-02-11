6795ee5faecdc62a779a64ee43ea50d3b493f1bb
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-02-11 16:25:26 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/mini2/riceFriend/RiceFriendApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.mini2.riceFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghae/mini2/riceFriend/RiceFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.mini2.riceFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiceFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
