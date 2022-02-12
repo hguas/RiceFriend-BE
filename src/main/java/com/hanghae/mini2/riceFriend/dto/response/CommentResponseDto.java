3e7cc2b64c696b702acd69fab1a888d3d6198e98
JEON <jeon34986@naver.com>
2022-02-12 19:03:43 +0900

    heesung : 맛집모임 목록조회, 맛집모임 정보조회 구현.

--- src/main/java/com/hanghae/mini2/riceFriend/dto/response/CommentResponseDto.java
+++ src/main/java/com/hanghae/mini2/riceFriend/dto/response/CommentResponseDto.java
@@ -2,11 +2,15 @@ package com.hanghae.mini2.riceFriend.dto.response;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
+@Builder
 @ApiModel(value = "댓글 정보", description = "댓글 정보 ResponseDto Class")
 public class CommentResponseDto {
     //COMMENT 테이블
@@ -15,5 +19,5 @@ public class CommentResponseDto {
     @ApiModelProperty(value = "댓글 내용")
     private String content;
     @ApiModelProperty(value = "댓글 작성일자")
-    private String createdAt;
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/hanghae/mini2/riceFriend/service/MeetingService.java
+++ src/main/java/com/hanghae/mini2/riceFriend/service/MeetingService.java
@@ -1,7 +1,12 @@
 package com.hanghae.mini2.riceFriend.service;
 
-import com.hanghae.mini2.riceFriend.dto.request.MeetingRequestDto;
+import com.hanghae.mini2.riceFriend.dto.response.CommentResponseDto;
 import com.hanghae.mini2.riceFriend.dto.response.MeetingDetailResponseDto;
+import com.hanghae.mini2.riceFriend.dto.response.MeetingResonseDto;
+import com.hanghae.mini2.riceFriend.dto.response.MeetingUserResponseDto;
+import com.hanghae.mini2.riceFriend.model.Comment;
+import com.hanghae.mini2.riceFriend.model.Meeting;
+import com.hanghae.mini2.riceFriend.model.MeetingUser;
 import com.hanghae.mini2.riceFriend.repository.MeetingRepository;
 import com.hanghae.mini2.riceFriend.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,37 +15,79 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class MeetingService {
 
-    private final RestaurantRepository restaurantRepository;
     private final MeetingRepository meetingRepository;
+    private final RestaurantRepository restaurantRepository;
 
+    // 맛집모임 목록 조회.
     @Transactional
-    public List<MeetingDetailResponseDto> findMeetingList() {
+    public List<MeetingResonseDto> findMeetingList() {
+//        List<Meeting> meetings = meetingRepository.findAllByOrderByCreatedAtDesc();
+//
+//        List<MeetingResonseDto> meetingResonseDtos = new ArrayList<>();
+//        for (Meeting meeting : meetings) {
+//            int commentCount = meeting.getComments().size();// 댓글개수
+//            int memberCount = meeting.getMeetingUsers().size();// 참여인원
+//
+//            MeetingResonseDto meetingResonseDto = MeetingResonseDto.builder()
+//                    .nickname(meeting.getUser().getNickname())
+//                    .userId(meeting.getUser().getId())
+//                    .imgUrl(meeting.getRestaurant().getImageUrl())
+//                    .locationId(meeting.getRestaurant().getLocation().getId())
+//                    .meetingId(meeting.getId())
+//                    .meetingTitle(meeting.getTitle())
+//                    .content(meeting.getContent())
+//                    .meetingDate(meeting.getDate())
+//                    .limitMember(meeting.getLimitMember())
+//                    .commentCount(commentCount)
+//                    .memberCount(memberCount)
+//                    .build();
+//
+//            meetingResonseDtos.add(meetingResonseDto);
+//        }
 
-
-
-        return new ArrayList<MeetingDetailResponseDto>();
+        return meetingRepository.findAllByOrderByCreatedAtDesc().stream().map(meeting -> meeting.toMeetingDetailResponseDto()).collect(Collectors.toList());
     }
 
+    // 맛집모임 정보를 조회.
     @Transactional
     public MeetingDetailResponseDto findMeeting(Long meeting_id) {
+        Meeting meeting = meetingRepository.findById(meeting_id).orElseThrow(
+                () -> new NullPointerException("해당 모임이 존재하지 않습니다."));
 
-        return new MeetingDetailResponseDto();
+        int commentCount = meeting.getComments().size();// 댓글개수
+        int memberCount = meeting.getMeetingUsers().size();// 참여인원
+
+        // 맛집모임 기본정보 셋팅
+        MeetingResonseDto meetingResonseDto = meeting.toMeetingDetailResponseDto();
+
+        // 맛집모임 댓글목록, 참여유저정보 셋팅
+        List<CommentResponseDto> commentResponseDtos = meeting.getComments().stream().map(comment -> comment.toCommentResponseDto()).collect(Collectors.toList());
+        List<MeetingUserResponseDto> meetingUserResponseDtos = meeting.getMeetingUsers().stream().map(meetingUser -> meetingUser.toMeetingUserResponseDto()).collect(Collectors.toList());
+
+        // 맛집모임 상세정보 셋팅
+        return MeetingDetailResponseDto.builder()
+                .meetingResonseDto(meetingResonseDto)
+                .commentResponseDtos(commentResponseDtos)
+                .meetingUserResponseDtos(meetingUserResponseDtos)
+                .build();
     }
 
-    @Transactional
-    public MeetingDetailResponseDto createMeeting(MeetingRequestDto requestDto, Long userId) {
-        return new MeetingDetailResponseDto();
-    }
+//    @Transactional
+//    public MeetingDetailResponseDto createMeeting(MeetingRequestDto requestDto, Long userId) {
+//        return new MeetingDetailResponseDto();
+//    }
 
-    @Transactional
-    public MeetingDetailResponseDto updateMeeting(Long meeting_id, MeetingRequestDto requestDto) {
-        return new MeetingDetailResponseDto();
-    }
+//    @Transactional
+//    public MeetingDetailResponseDto updateMeeting(Long meeting_id, MeetingRequestDto requestDto) {
+//        return new MeetingDetailResponseDto();
+//    }
 
     @Transactional
     public void deleteMeeting(Long meeting_id) {
