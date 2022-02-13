380bb55db6d4b2eda4c336999b420b3aebe048e7
JEON <jeon34986@naver.com>
2022-02-13 21:25:07 +0900

    heesung : 맛집모임 전체조회, 맛집모임 상세조회 테스트완료.. , 댓글 강제주입 쿼리작성

---
+++ src/main/java/com/hanghae/mini2/insert_comment.sql
@@ -0,0 +1,15 @@
+## 댓글_TABLLE(ID,MEETING_ID,USER_ID,CONTENT) ##
+INSERT INTO PRAC.COMMENT(ID,MEETING_ID,USER_ID,CONTENT)
+VALUES
+    (1, '1','1','test1_댓글'),
+    (2, '1','2','test2_댓글'),
+    (3, '1','3','test3_댓글'),
+    (4, '1','4','test4_댓글'),
+    (5, '1','5','test5_댓글'),
+    (6, '1','6','test6_댓글'),
+    (7, '1','7','test7_댓글'),
+    (8, '1','8','test8_댓글'),
+    (9, '1','9','test9_댓글'),
+    (10, '1','10','test10_댓글');
+
+SELECT * FROM PRAC.COMMENT;
