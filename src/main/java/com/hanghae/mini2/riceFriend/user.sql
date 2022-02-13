0848bc3ec634866f6f8fad18ec0ccd5bc253c6ff
JEON <jeon34986@naver.com>
2022-02-13 20:42:25 +0900

    heesung : user정보 강제주입 SQL

---
+++ src/main/java/com/hanghae/mini2/riceFriend/user.sql
@@ -0,0 +1,15 @@
+## 유저_TABLLE(ID,NAME) ##
+INSERT INTO PRAC.USER(ID,NICKNAME,EMAIL,PASSWORD,GENDER)
+VALUES
+    (1, 'test1','test1@naver.com','test1','남성'),
+    (2, 'test2','test2@naver.com','test2','여성'),
+    (3, 'test3','test3@naver.com','test3','남성'),
+    (4, 'test4','test4@naver.com','test4','여성'),
+    (5, 'test5','test5@naver.com','test5','남성'),
+    (6, 'test6','test6@naver.com','test6','여성'),
+    (7, 'test7','test7@naver.com','test7','남성'),
+    (8, 'test8','test8@naver.com','test8','여성'),
+    (9, 'test9','test9@naver.com','test9','남성'),
+    (10, 'test10','test10@naver.com','test10','여성');
+
+SELECT * FROM PRAC.USER
