de8693360312744c6455f50bdee19dff8cbeda8f
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-02-14 18:45:16 +0900

    byungmin : CORS 로직 추가

--- src/main/java/com/hanghae/mini2/riceFriend/controller/UserApiController.java
+++ src/main/java/com/hanghae/mini2/riceFriend/controller/UserApiController.java
@@ -6,10 +6,7 @@ import com.hanghae.mini2.riceFriend.dto.response.CMResponseDto;
 import com.hanghae.mini2.riceFriend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
